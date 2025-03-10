378da4e76a77dbcd21d09665d27739a489f24f29
Sudarshan Hingalje <sudarshanhingalje1@gmail.com>
2025-03-11 03:15:10 +0530

    footer section updated

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-placeholder").innerHTML = data;
+        });
+});
+console.log("JavaScript is working!");
